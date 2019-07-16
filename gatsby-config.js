f284044219b4cb165a09a37593be4916cf8ceeeb
pmontens <pieterjan@montens.net>
2019-07-16 15:25:18 +0200

    Added favicon :ok_hand:

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-favicon`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
