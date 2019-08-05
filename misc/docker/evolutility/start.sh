5b6e2d5e69cf0083dae1fbf89d62ac5de93e046f
pmontens <pieterjan@montens.net>
2019-08-05 14:25:22 +0200

    env variable test

--- misc/docker/evolutility/start.sh
+++ misc/docker/evolutility/start.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-echo -e "DANGEROUSLY_DISABLE_HOST_CHECK=true" > /src/ui/.env.development.local
+echo -e "DANGEROUSLY_DISABLE_HOST_CHECK=true\n" > /src/ui/.env.development.local
 pm2 start ./server/bin/www
 cd ./ui
 npm start
