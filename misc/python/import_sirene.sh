82cf2d26c9373a2518675648c9e77a450f58b573
pmontens <pieterjan@montens.net>
2019-11-27 14:05:34 +0100

    import script tweaking

--- misc/python/import_sirene.sh
+++ misc/python/import_sirene.sh
@@ -5,6 +5,6 @@
     -f "etatadministratifetablissement==A" \
     -f "caractereemployeuretablissement==O" \
     ./raw_data/StockEtablissement_utf8.csv parse | \
-dd obs=1024M | \
-parallel --pipe ./json2db.py --module siren --tag csv_siren_datagouv --config_file config.yaml
+dd obs=50M | \
+parallel --max-lines=5000 --pipe ./json2db.py --module siren --tag csv_siren_datagouv --config_file config.yaml
 
