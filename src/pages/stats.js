8447503493d094cee2223ae7463e9359b7beb506
pmontens <pieterjan@montens.net>
2020-03-24 21:58:28 +0100

    using react hook to store iframe url

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,23 +1,27 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 
-const StatsPage = () => {
+const METABASE_SITE_URL = "https://andi.beta.gouv.fr/metabase";
+const METABASE_SECRET_KEY = "cf2cdd6614efbeea460a916373ae8b02f7a5da4f8090744d826a637327b2f94a";
 
-    var jwt = require("jsonwebtoken");
-    
-    var METABASE_SITE_URL = "https://andi.beta.gouv.fr/metabase";
-    var METABASE_SECRET_KEY = "cf2cdd6614efbeea460a916373ae8b02f7a5da4f8090744d826a637327b2f94a";
-    
-    var payload = {
-      resource: { question: 11 },
-      params: {},
-      exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
-    };
-    var token = jwt.sign(payload, METABASE_SECRET_KEY);
-    
-    var iframeUrl = METABASE_SITE_URL + "/embed/question/" + token + "#bordered=true&titled=true";
+const StatsPage = () => {
+    const [iframeUrl, setIframeUrl] = useState(false);
+
+    useEffect( () => {
+        var jwt = require("jsonwebtoken");
+        
+        var payload = {
+          resource: { question: 11 },
+          params: {},
+          exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
+        };
+
+        var token = jwt.sign(payload, METABASE_SECRET_KEY);
+        
+        setIframeUrl(METABASE_SITE_URL + "/embed/question/" + token + "#bordered=true&titled=true");
+    }, [] );
 
     return (
     <Layout>
