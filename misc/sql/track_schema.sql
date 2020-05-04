b925260ee62d20041db127fc3a6ae87eda8eb190
pmontens <pieterjan@montens.net>
2020-05-04 17:39:49 +0200

    :bookmark: ajout indexes pour les vues metabase

--- misc/sql/track_schema.sql
+++ misc/sql/track_schema.sql
@@ -1,5 +1,10 @@
 DROP INDEX IF EXISTS idx_trackers_sid;
 DROP INDEX IF EXISTS idx_trackers_created;
+DROP INDEX IF EXISTS idx_trackers_dev;
+DROP INDEX IF EXISTS idx_trackers_page;
+DROP INDEX IF EXISTS idx_trackers_action;
+DROP INDEX IF EXISTS idx_trackers_question;
+DROP INDEX IF EXISTS idx_trackers_visible;
 DROP TABLE IF EXISTS "trackers";
 
 CREATE TABLE trackers (
@@ -12,3 +17,12 @@ CREATE TABLE trackers (
 );
 CREATE INDEX idx_trackers_sid ON trackers USING BTREE (session_id);
 CREATE INDEX idx_trackers_created ON trackers USING BTREE (date_created);
+-- Indices for generic metabase views
+CREATE INDEX idx_trackers_dev ON trackers USING BTREE ((data->'meta'->>'dev')) WHERE data->'meta'->>'dev' = 'false';
+CREATE INDEX idx_trackers_page ON trackers USING BTREE ((data->>'page')) WHERE (data->'page') IS NOT NULL;
+CREATE INDEX idx_trackers_action ON trackers USING BTREE ((data->>'action')) WHERE (data->'action') IS NOT NULL;
+
+-- Indices for specific metabase queries
+CREATE INDEX idx_trackers_question ON trackers USING BTREE ((data->'meta'->>'question')) WHERE (data->'meta'->'question') IS NOT NULL;
+CREATE INDEX idx_trackers_visible ON trackers USING BTREE ((data->'meta'->>'visible')) WHERE (data->'meta'->'visible') IS NOT NULL;
+
