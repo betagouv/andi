2d45eaf27727179d6d35aa82354004a1173a943a
pmontens <pieterjan@montens.net>
2020-05-13 09:30:35 +0200

    :wrench: fixed bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,12 +36,12 @@ export const query = graphql`
     }
 }
 `
-const FormElement = ({name, text, onclick, type}) => (
-    <div className="form__group">
-      <label htmlFor={ name }>{ text }</label>
-      <input name={ name } id={ name } type={ type }required onClick={ onclick }/>
-    </div>
-)
+// const FormElement = ({name, text, onclick, type}) => (
+//     <div className="form__group">
+//       <label htmlFor={ name }>{ text }</label>
+//       <input name={ name } id={ name } type={ type }required onClick={ onclick }/>
+//     </div>
+// )
 
 // const abbr_andi = "accompagnement numérique au développement de l'insertion"
 
@@ -169,67 +169,66 @@ class IndexPage extends React.Component {
     render() {
         return (
              <Layout title="Accueil ANDi" showNav={ true } role={ false }>
-             <div className="container-fluid covid" style={{ margin: '.5rem 0rem 1rem 0rem',  padding: '1rem 0rem 1rem 0rem', zIndex:100, position:'relative'}} role="banner">
-            <main role="main" style={{zIndex:100}}>
-                <Hero title={ this.d.titre } subtitle={ this.d.titre_description} text={this.d.slogan} button={this.d.bouton} />
-                                {/* <div className="svg_container" aria-hidden="true" focusable="false">
-                  <svg className="svg_1" viewBox="0 70 500 80" preserveAspectRatio="none">
-                    <rect x={0} y={0} width={500} height={500} style={{stroke: 'none'}} />
-                    <path d="M0,100 C150,0 350,250 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}} />
-                  </svg>
-                </div> */}
-                <section>
-                  <div className="container-fluid" style={{marginTop: '2rem', marginBottom: '1rem'}}>
-                    <div className="row numlist">
-                      <div className="col-xl-10 offset-xl-1 offset-0 col-12">
-                        <h2 className="section__title " style={{marginTop: '0rem'}}>{ this.d.soustitre1 }</h2>
-                        <div className="row mt-0">
-                            <ImmersionDetails details={ this.definitions } />
+                <main role="main" style={{zIndex:100}}>
+                    <Hero title={ this.d.titre } subtitle={ this.d.titre_description} text={this.d.slogan} button={this.d.bouton} />
+                                    {/* <div className="svg_container" aria-hidden="true" focusable="false">
+                      <svg className="svg_1" viewBox="0 70 500 80" preserveAspectRatio="none">
+                        <rect x={0} y={0} width={500} height={500} style={{stroke: 'none'}} />
+                        <path d="M0,100 C150,0 350,250 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}} />
+                      </svg>
+                    </div> */}
+                    <section>
+                      <div className="container-fluid" style={{marginTop: '2rem', marginBottom: '1rem'}}>
+                        <div className="row numlist">
+                          <div className="col-xl-10 offset-xl-1 offset-0 col-12">
+                            <h2 className="section__title " style={{marginTop: '0rem'}}>{ this.d.soustitre1 }</h2>
+                            <div className="row mt-0">
+                                <ImmersionDetails details={ this.definitions } />
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
 
-                  <div className="container-fluid" style={{marginTop: '2rem', marginBottom: '1rem'}}>
-                    <div className="row numlist">
-                      <div className="col-md-10 offset-xl-1 offset-0 col-12">
-                        <h2 className="section__title" style={{marginTop: '0rem'}}>{ this.d.soustitre2 }</h2>
-                        <div className="row mt-0">
-                          <PointDetail image={ tickbox } title={ this.d.point1 } text={ this.d.point1_texte } />
-                          <PointDetail image={ glass } title={ this.d.point2 } text={ this.d.point2_texte } />
-                          <PointDetail image={ talk } title={ this.d.point3 } text={ this.d.point3_texte } />
+                      <div className="container-fluid" style={{marginTop: '2rem', marginBottom: '1rem'}}>
+                        <div className="row numlist">
+                          <div className="col-md-10 offset-xl-1 offset-0 col-12">
+                            <h2 className="section__title" style={{marginTop: '0rem'}}>{ this.d.soustitre2 }</h2>
+                            <div className="row mt-0">
+                              <PointDetail image={ tickbox } title={ this.d.point1 } text={ this.d.point1_texte } />
+                              <PointDetail image={ glass } title={ this.d.point2 } text={ this.d.point2_texte } />
+                              <PointDetail image={ talk } title={ this.d.point3 } text={ this.d.point3_texte } />
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
 
-                  <br />
-                  <br />
-                </section>
-                <div className="svg_container" aria-hidden="true" focusable="false">
-                  <svg className="svg_2" viewBox="0 70 500 60" preserveAspectRatio="none">
-                    <rect x={0} y={0} width={500} height={500} style={{stroke: 'none'}} />
-                    <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}} />
-                  </svg>
-                </div>
-                <section id="a-propos" className="section-grey" style={{marginTop: '1rem', paddingBottom: '8rem'}}>  
-                  <div className="container-fluid" style={{display: 'inline-block'}}>
-                    <div className="row no-gutters">
-                      <div className="col-xl-10 offset-xl-1 col-12">
-                        <div className="row">
-                          <div className="col-lg-5 col-xs-12 align-self-center">
-                              <h2>{ this.d.soustitrequi }</h2>
-                              <div>{ this.d.quinous }</div>
-                          </div>
-                          <div className="col-lg-7 col-xs-12 align-self-center text-center no-gutters">
-                            <img className="opt_img team-startup-illu" src={team_illu} alt="" srcSet={`${team_illu_2x} 2x, ${team_illu_3x} 3x`} />
-                          </div>
-                          </div>
-                        </div>
-                      </div> 
+                      <br />
+                      <br />
+                    </section>
+                    <div className="svg_container" aria-hidden="true" focusable="false">
+                      <svg className="svg_2" viewBox="0 70 500 60" preserveAspectRatio="none">
+                        <rect x={0} y={0} width={500} height={500} style={{stroke: 'none'}} />
+                        <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: 'none'}} />
+                      </svg>
                     </div>
-                </section>
-            </main>
+                    <section id="a-propos" className="section-grey" style={{marginTop: '1rem', paddingBottom: '8rem'}}>  
+                      <div className="container-fluid" style={{display: 'inline-block'}}>
+                        <div className="row no-gutters">
+                          <div className="col-xl-10 offset-xl-1 col-12">
+                            <div className="row">
+                              <div className="col-lg-5 col-xs-12 align-self-center">
+                                  <h2>{ this.d.soustitrequi }</h2>
+                                  <div>{ this.d.quinous }</div>
+                              </div>
+                              <div className="col-lg-7 col-xs-12 align-self-center text-center no-gutters">
+                                <img className="opt_img team-startup-illu" src={team_illu} alt="" srcSet={`${team_illu_2x} 2x, ${team_illu_3x} 3x`} />
+                              </div>
+                              </div>
+                            </div>
+                          </div> 
+                        </div>
+                    </section>
+                </main>
             </Layout>
     )
     }
